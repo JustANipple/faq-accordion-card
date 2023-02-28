bb4c62405cdbc219146b192332aba4ad944fd300
JustANipple <prandisamuel99@gmail.com>
2023-02-28 14:45:15 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -6,63 +6,27 @@ const boxArray = document.querySelectorAll([
     ".support-box"
 ]);
 
-/*
-const questionArray = document.querySelectorAll([
-    ".invite-question",
-    ".size-question",
-    ".password-question",
-    ".sub-question",
-    ".support-question"
-]);
-const answerArray = document.querySelectorAll([
-    ".invite-answer",
-    ".size-answer",
-    ".password-answer",
-    ".sub-answer",
-    ".support-answer"
-]);
-const arrowArray = document.querySelectorAll([
-    ".invite-arrow",
-    ".size-arrow",
-    ".password-arrow",
-    ".sub-arrow",
-    ".support-arrow" 
-]);
-*/
-
 const rootStyles = window.getComputedStyle(document.documentElement);
 const fwReg = rootStyles.getPropertyValue("--fw-reg");
 const fwBold = rootStyles.getPropertyValue("--fw-bold");
-
-/*
-for(let i = 0; i < boxArray.length; i++) {
-    boxArray[i].addEventListener("click", (event) => {
-
-        if(window.getComputedStyle(answerArray[i]).display === "none") {
-            for(let j = 0; j < boxArray.length; j++) {
-                answerArray[j].style.display = "none";
-                questionArray[j].style.fontWeight = fwReg;
-                arrowArray[j].style.transform = "rotate(0deg)";
-            }
-            answerArray[i].style.display = "block";
-            questionArray[i].style.fontWeight = fwBold;
-            arrowArray[i].style.transform = "rotate(180deg)";
-        } else {
-            answerArray[i].style.display = "none";
-            questionArray[i].style.fontWeight = fwReg;
-            arrowArray[i].style.transform = "rotate(0deg)";
-        }
-
-    });
-}
-*/
 
 for(const box of boxArray) {
     box.addEventListener("click", showOrNot);
 }
 
 function showOrNot() {
-    if(window.getComputedStyle(this.children[2]).display === "block") {
-        
-    } else if (window.getComputedStyle(this.children[2]).display === "none")
+    if(window.getComputedStyle(this.children[2]).display === "none") {
+        for(const box of boxArray) {
+            this.children[0].style.fontWeight = fwReg;
+            this.children[1].style.transform = "rotate(0deg)";
+            this.children[2].style.display = "none";      
+        }        
+        this.children[0].style.fontWeight = fwBold;
+        this.children[1].style.transform = "rotate(180deg)";
+        this.children[2].style.display = "block";
+    } else {
+        this.children[0].style.fontWeight = fwReg;
+        this.children[1].style.transform = "rotate(0deg)";
+        this.children[2].style.display = "none";  
+    }
 }
