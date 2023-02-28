dbfcc8f518422958395a941edd02c7c445f9d1ae
JustANipple <prandisamuel99@gmail.com>
2023-02-28 13:13:13 +0100

    modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const boxArray = document.querySelectorAll([
     ".sub-box",
     ".support-box"
 ]);
+const questionArray = document.querySelectorAll([
+    ".invite-question",
+    ".size-question",
+    ".password-question",
+    ".sub-question",
+    ".support-question"
+]);
 const answerArray = document.querySelectorAll([
     ".invite-answer",
     ".size-answer",
@@ -12,16 +19,34 @@ const answerArray = document.querySelectorAll([
     ".sub-answer",
     ".support-answer"
 ]);
+const arrowArray = document.querySelectorAll([
+    ".invite-arrow",
+    ".size-arrow",
+    ".password-arrow",
+    ".sub-arrow",
+    ".support-arrow" 
+]);
+const rootStyles = window.getComputedStyle(document.documentElement);
+const fwReg = rootStyles.getPropertyValue("--fw-reg");
+const fwBold = rootStyles.getPropertyValue("--fw-bold");
 
 for(let i = 0; i < boxArray.length; i++) {
     boxArray[i].addEventListener("click", (event) => {
+
         if(window.getComputedStyle(answerArray[i]).display === "none") {
-            for(const answer of answerArray) {
-                answer.style.display = "none";
+            for(let j = 0; j < boxArray.length; j++) {
+                answerArray[j].style.display = "none";
+                questionArray[j].style.fontWeight = fwReg;
+                arrowArray[j].style.transform = "rotate(0deg)";
             }
             answerArray[i].style.display = "block";
+            questionArray[i].style.fontWeight = fwBold;
+            arrowArray[i].style.transform = "rotate(180deg)";
         } else {
             answerArray[i].style.display = "none";
+            questionArray[i].style.fontWeight = fwReg;
+            arrowArray[i].style.transform = "rotate(0deg)";
         }
+
     });
 }
