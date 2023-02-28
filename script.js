aba73c11e375f8fc6d35cbb0c3635c851a04f19f
JustANipple <prandisamuel99@gmail.com>
2023-02-28 14:52:14 +0100

    modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Selecting every question box and saving it into an Array
 const boxArray = document.querySelectorAll([
     ".invite-box",
     ".size-box",
@@ -6,20 +7,27 @@ const boxArray = document.querySelectorAll([
     ".support-box"
 ]);
 
+//Selecting font weights into root element to change weights on click
 const rootStyles = window.getComputedStyle(document.documentElement);
 const fwReg = rootStyles.getPropertyValue("--fw-reg");
 const fwBold = rootStyles.getPropertyValue("--fw-bold");
 
+//Assigning to every box the event when it gets clicked
 for(const box of boxArray) {
     box.addEventListener("click", showOrNot);
 }
 
+//Function to show or hide faqs
 function showOrNot() {
+    //with children i get an array of all non-text elements into the saved box
+    //children[0] = question box
+    //children[1] = arrow box
+    //children[2] = answer box
     if(window.getComputedStyle(this.children[2]).display === "none") {
         for(const box of boxArray) {
-            this.children[0].style.fontWeight = fwReg;
-            this.children[1].style.transform = "rotate(0deg)";
-            this.children[2].style.display = "none";      
+            box.children[0].style.fontWeight = fwReg;
+            box.children[1].style.transform = "rotate(0deg)";
+            box.children[2].style.display = "none";      
         }        
         this.children[0].style.fontWeight = fwBold;
         this.children[1].style.transform = "rotate(180deg)";
